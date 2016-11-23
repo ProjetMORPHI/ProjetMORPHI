d0595d2bfadab6e11e01e13b2921e5cd445a706d
unknown <e1595444@FC411-MJPTCLY.cmaisonneuve.qc.ca>
2016-11-23 13:44:28 -0500

    ajout de arret dans état gazeux et message pour tuers les ennemis

--- unity/Assets/Script/Ennemies/fantome.cs
+++ unity/Assets/Script/Ennemies/fantome.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class fantome : MonoBehaviour {
@@ -12,15 +13,21 @@ public class fantome : MonoBehaviour {
 	private float unitMax=0.0f; // La variable pour calculer la position maximum de fantôme 
 	private float unitMin=0.0f; // La variable pour calculer la position minimum de fantôme 
 
+	private Vector3 enemieForce;// La variable pour reculer le personnage quand il touche le fantôme
+
 	private bool positionPositiveX = true; 
+
+
+	public GameObject HandleVie;// Handle pour le controle de la bar de vie
+	public float dommageEnnemie;
 
 	// Use this for initialization
 	void Start () {
-		
+
 		posX = this.transform.position.x; // prendre la position initial sur l'axe x de fantome
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -51,11 +58,27 @@ public class fantome : MonoBehaviour {
 		}
 
 		this.transform.position = new Vector3(posX, this.transform.position.y, this.transform.position.z);
-	
+
 	}
-    void OnTriggerEnter(Collider col)
-    {
-        SceneManager.LoadScene("Tutoriel", LoadSceneMode.Single);
-       // SceneManager.LoadScene(SceneManager.GetActiveScene());
-    }
+	void OnCollisionEnter(Collision monCollision)
+	{
+
+		GestionEtat joueurEtat = monCollision.gameObject.GetComponent<GestionEtat>();
+		enemieForce.Set((monCollision.contacts [0].normal.x) * -80, (monCollision.contacts [0].normal.y) * -5,0);
+		monCollision.rigidbody.AddForce(enemieForce,ForceMode.Impulse);
+		if (HandleVie.activeSelf == false) {//si le jouer a tout ce energie au complet
+
+			HandleVie.SetActive (true);//activation de handle pour simuler la premiere touche
+			GameObject vie = GameObject.FindGameObjectWithTag("BarVie") as GameObject;
+			Scrollbar barVie = vie.GetComponent<Scrollbar> ();
+			barVie.size = barVie.size + dommageEnnemie;
+		} 
+		else 
+		{//si le joueur a deja de dommage on va ajouter plus de dommage
+			GameObject vie = GameObject.FindGameObjectWithTag("BarVie") as GameObject;
+			Scrollbar barVie = vie.GetComponent<Scrollbar> ();
+			barVie.size = barVie.size + dommageEnnemie;
+		}
+
+	}
 }
