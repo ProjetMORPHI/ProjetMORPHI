83b767ed0ed8b22d833e8bc8650ee865157b9fba
Nicolas <e1595221@FC411-MJPTCLR.cmaisonneuve.qc.ca>
2016-11-30 10:27:27 -0500

    Script pour platforme rotative

--- unity/Assets/Script/Environnement/Tourniquet.cs
+++ unity/Assets/Script/Environnement/Tourniquet.cs
@@ -3,19 +3,32 @@ using System.Collections;
 
 public class Tourniquet : MonoBehaviour {
 
+	//le point autour du quel la plateforme tourne
 	public GameObject pivot;
+
+	//varialble pour stocker la rotation initiale de la platforme
+	private Quaternion rotInitiale;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		
+		// on stock la rotation initiale de la platforme
+		rotInitiale = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		// on stock la position du point de piveau sous la forme d'un vector3
 		Vector3 positionPivot = pivot.transform.position;
 
-		transform.RotateAround(positionPivot,positionPivot.z,45*Time.deltaTime); //fait pivoter le game object de 45 degrés par seconde
+		//on s'assure que la rotation de la platforme reste toujours la même quand elle tourne autour du pivot
+		transform.rotation = rotInitiale;
+
+		//fait pivoter la plateforme de 45 degrés par seconde 
+		//autour du point de pivot 
+		//sur l'axe des Z représenté par Vector3.forward
+		transform.RotateAround(positionPivot,Vector3.forward,45*Time.deltaTime);
 	}
 }
