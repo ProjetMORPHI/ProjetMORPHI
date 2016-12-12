b61a4af5c3e668e5d792be7a311513e395dd6701
Leonardo Alfonso Lindarte Caceres <Leonardo Alfonso Lindarte Caceres>
2016-12-12 02:17:39 -0500

    fin du programmation du niveau1

---
+++ unity/Assets/Script/Environnement/ButtonGrandCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonGrandCollider : MonoBehaviour {
+
+	public Animation animButton;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider monCollider){
+
+		if (monCollider.gameObject.name == "BalleLourde") {
+
+			animButton.Play ();//la animation de appuyer le button fonctionne seulement si le personnage est en état solide
+		}
+
+
+	}
+}
