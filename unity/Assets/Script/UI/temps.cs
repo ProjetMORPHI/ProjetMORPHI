dee8a7e0c9d0bad4061acec41cdfbf09bfdc5bc0
unknown <e1595433@FC411-MJPTCLF.cmaisonneuve.qc.ca>
2017-01-09 11:47:34 -0500

    UI elements

---
+++ unity/Assets/Script/UI/temps.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// http://answers.unity3d.com/questions/45676/making-a-timer-0000-minutes-and-seconds.html
+public class temps : MonoBehaviour {
+
+	public Text tempsText;
+    private float minuteur = 00f;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        minuteur += Time.deltaTime;
+
+
+
+
+        string minutes = Mathf.Floor(minuteur / 60).ToString("00");
+        string seconds = (minuteur % 60).ToString("00");
+        tempsText.text = minutes + ":" + seconds;
+
+	}
+}
