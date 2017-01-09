e5a991e64e2de2851e68cfb849f227bf36f29eb3
Nicolas <e1595221@FC411-MJPTCLR.cmaisonneuve.qc.ca>
2017-01-09 11:27:28 -0500

    Quand on meurt on ne revient plus au tutoriel. J'ai changée l'ordre des niveau 1 et 2

---
+++ unity/Assets/Script/UI/Bouton_Simple.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Bouton_Simple : MonoBehaviour {
+
+	//Ce script et pour une navigation simple dans le menu
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//envoie à la scène dont le nom est spécifié
+	public void ChargeSceneParNom(string nomScene)
+	{
+
+		SceneManager.LoadScene (nomScene); 
+
+	}
+
+
+}
