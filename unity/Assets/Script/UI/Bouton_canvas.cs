44ceeebb0b766a427545e25143c28f39b16b3eef
unknown <e1595433@FC411-MJPTCLF.cmaisonneuve.qc.ca>
2017-01-16 10:38:11 -0500

    l'ajout de UI dans les niveaux

---
+++ unity/Assets/Script/UI/Bouton_canvas.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Bouton_canvas : MonoBehaviour {
+
+
+
+    public GameObject panelReglage;
+    private GameObject pausePlayBouton;
+    public GameObject panelAid;
+    
+    public Sprite pauseImage;
+    public Sprite playImage;
+    private bool paused;
+    private bool ReglageIsOn;
+    private bool aidIsOn;
+
+
+    // Use this for initialization
+    void Start () {
+        pausePlayBouton = GameObject.Find("ButtonPausePlay");
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void buttonPlayPause()
+    {
+
+        if (ReglageIsOn != true && aidIsOn != true)
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                pausePlayBouton.GetComponent<Image>().sprite = playImage;
+                paused = true;
+
+
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pausePlayBouton.GetComponent<Image>().sprite = pauseImage;
+                paused = false;
+            }
+        }
+    }
+    //***************************** Ouvrir et Fermer le paneau reglage*************************************
+    public void buttonReglage()
+    {
+        if(paused != true && aidIsOn != true)
+        {
+            Time.timeScale = 0;
+            panelReglage.SetActive(true);
+            ReglageIsOn = true;
+
+        }
+        
+
+    }
+    public void buttonReglageFermer()
+    {
+        Time.timeScale = 1;
+        panelReglage.SetActive(false);
+        ReglageIsOn = false;
+
+    }
+
+    //***************************** Ouvrir et Fermer le paneau aid*************************************
+    public void buttonAid()
+    {
+        if (paused != true && ReglageIsOn != true)
+        {
+            Time.timeScale = 0;
+            panelAid.SetActive(true);
+            aidIsOn = true;
+
+        }
+
+
+    }
+    public void buttonAidFermer()
+    {
+        Time.timeScale = 1;
+        panelAid.SetActive(false);
+        aidIsOn = false;
+
+    }
+}
+
+internal class Button
+{
+    internal object image;
+}
